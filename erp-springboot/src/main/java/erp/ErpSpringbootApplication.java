f332bc315d7e9c76df2901f9f9c4445e418dfb25
ahmedbakhrouf <ahmedba5rouf@gmail.com>
2018-04-03 20:52:11 +0100

    First commit for the spring project

---
+++ erp-springboot/src/main/java/erp/ErpSpringbootApplication.java
@@ -0,0 +1,12 @@
+package erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpSpringbootApplication.class, args);
+	}
+}
